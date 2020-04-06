312c55b29feb332b3f2870613ea902d5fb8a0383
Ryan McFadden <ryanmcfadden123@gmail.com>
2020-04-06 22:58:08 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,3 +1,23 @@
 class Post
+  attr_accessor :name
+  attr_reader :author
+  @@all = []
   
+  def initialize(name)
+    @name = name 
+    @@all << self
+  end
+  
+  def self.all 
+    @@all
+  end 
+  
+  def author= (author)
+    @author = author
+    @author.posts << self
+  end
+  
+  def author_name
+    @author.name if !!@author
+  end
 end
